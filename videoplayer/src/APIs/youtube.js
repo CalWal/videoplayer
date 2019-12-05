99647c7a703e29c9d60267fcc06f64453d7b6e72
CalWal <callumtrwallace@gmail.com>
2019-12-05 18:32:06 +1000

    added axios

--- videoplayer/src/APIs/youtube.js
+++ videoplayer/src/APIs/youtube.js
@@ -1,1 +1,12 @@
+import axios from "axios";
+
 const KEY = "AIzaSyC6AMyJQamEchz-vAcxncuLO-Z1djuAjME";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: 5,
+    key: KEY
+  }
+});
