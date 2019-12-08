62fc5683d33a28b3594261a96a78ee951f70d0d3
CalWal <callumtrwallace@gmail.com>
2019-12-08 12:30:25 +1000

    Updates

---
+++ precache-manifest.94d4e93d25080f846f9258b4450814a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55ee59db4f34a61e56a0ddee1e883f36",
+    "url": "/videoplayer/index.html"
+  },
+  {
+    "revision": "6a31fa6f870f3602b7b9",
+    "url": "/videoplayer/static/css/main.4a966e5b.chunk.css"
+  },
+  {
+    "revision": "d2b57a4d474126d955d7",
+    "url": "/videoplayer/static/js/2.565fa8f2.chunk.js"
+  },
+  {
+    "revision": "6a31fa6f870f3602b7b9",
+    "url": "/videoplayer/static/js/main.6b304b51.chunk.js"
+  },
+  {
+    "revision": "c03b5bb90a1902eeeb6d",
+    "url": "/videoplayer/static/js/runtime-main.9e9b5660.js"
+  }
+]);
